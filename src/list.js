00826d6e93aad47069200ee177652b15f8b7d758
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-08-03 09:28:17 -0400

    Finished list class + list manager factory

--- src/list.js
+++ src/list.js
@@ -1,6 +1,6 @@
 // List class
 
-class List {
+export class List {
     constructor(name) {
         this.name = name;
         this.toDos = [];
@@ -17,4 +17,41 @@ class List {
     removeToDo(unwantedToDo) {
         this.toDos.splice(this.toDos.indexOf(unwantedToDo), 1);
     }
+
+    getCompletedToDos() {
+        const completedToDos = [];
+        for (const toDo of this.toDos) {
+            if (toDo.isComplete === "Yes") {
+                completedToDos.push(toDo);
+            } 
+        }
+        return completedToDos;
+    }
+
+    getIncompleteToDos() {
+        const incompleteToDos = [];
+        for (const toDo of this.toDos) {
+            if (toDo.isComplete === "No") {
+                incompleteToDos.push(toDo);
+            }
+        }
+        return incompleteToDos;
+    }
 }
+
+export const lists = (function listManager() {
+    const allLists = [];
+
+    function addNewList(newList) {
+        allLists.push(newList);
+    }
+
+    function getAllLists() {
+        return allLists;
+    }
+
+    return {
+        addNewList,
+        getAllLists
+    }
+})();
