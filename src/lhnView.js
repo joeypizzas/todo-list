f8137fe690d537166e0d129171e106de242b338f
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-08-15 08:27:25 -0400

    Added event listeners on home and list buttons

---
+++ src/lhnView.js
@@ -0,0 +1,35 @@
+// JS for LHN 
+
+export function initLHNListeners() {
+    const root = document.documentElement;
+    
+    const homeButtonLHN = document.querySelector("#home-button-lhn");
+    homeButtonLHN.addEventListener("mouseover", () => {
+        homeButtonLHN.style.backgroundColor = getComputedStyle(root).getPropertyValue("--lhn-hover");
+    });
+    homeButtonLHN.addEventListener("mouseout", () => {
+        homeButtonLHN.style.backgroundColor = getComputedStyle(root).getPropertyValue("--lhn");
+    });
+    homeButtonLHN.addEventListener("mousedown", () => {
+        homeButtonLHN.style.backgroundColor = getComputedStyle(root).getPropertyValue("--lhn-click");
+    });
+    homeButtonLHN.addEventListener("mouseup", () => {
+        homeButtonLHN.style.backgroundColor = getComputedStyle(root).getPropertyValue("--lhn-hover");
+    });
+
+    const listItemsLHN = document.querySelectorAll(".item-lhn-child");
+    listItemsLHN.forEach((list) => {
+        list.addEventListener("mouseover", () => {
+            list.style.backgroundColor = getComputedStyle(root).getPropertyValue("--lhn-hover");
+        });
+        list.addEventListener("mouseout", () => {
+            list.style.backgroundColor = getComputedStyle(root).getPropertyValue("--lhn");
+        });
+        list.addEventListener("mousedown", () => {
+            list.style.backgroundColor = getComputedStyle(root).getPropertyValue("--lhn-click");
+        });
+        list.addEventListener("mouseup", () => {
+            list.style.backgroundColor = getComputedStyle(root).getPropertyValue("--lhn-hover");
+        });
+    });
+}
