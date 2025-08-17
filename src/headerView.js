9fc54bcc3ea5ce48c88166502a2e6a39c75eea50
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-08-17 17:02:50 -0400

    Updated new button hover + click colors

--- src/headerView.js
+++ src/headerView.js
@@ -6,7 +6,7 @@ export function initHeaderListeners() {
     const plusSVG = document.querySelector(".plus-svg");
 
     homeNewButton.addEventListener("mouseover", () => {
-        homeNewButton.style.backgroundColor = getComputedStyle(root).getPropertyValue("--header");
+        homeNewButton.style.backgroundColor = getComputedStyle(root).getPropertyValue("--header-hover");
         homeNewButton.style.color = getComputedStyle(root).getPropertyValue("--header-text");
         plusSVG.style.stroke = getComputedStyle(root).getPropertyValue("--header-text");
     });
@@ -16,12 +16,12 @@ export function initHeaderListeners() {
         plusSVG.style.stroke = getComputedStyle(root).getPropertyValue("--header");
     });
     homeNewButton.addEventListener("mousedown", () => {
-        homeNewButton.style.backgroundColor = getComputedStyle(root).getPropertyValue("--header-text");
-        homeNewButton.style.color = getComputedStyle(root).getPropertyValue("--header");
-        plusSVG.style.stroke = getComputedStyle(root).getPropertyValue("--header");
+        homeNewButton.style.backgroundColor = getComputedStyle(root).getPropertyValue("--header-click");
+        homeNewButton.style.color = getComputedStyle(root).getPropertyValue("--header-text");
+        plusSVG.style.stroke = getComputedStyle(root).getPropertyValue("--header-text");
     });
     homeNewButton.addEventListener("mouseup", () => {
-        homeNewButton.style.backgroundColor = getComputedStyle(root).getPropertyValue("--header");
+        homeNewButton.style.backgroundColor = getComputedStyle(root).getPropertyValue("--header-hover");
         homeNewButton.style.color = getComputedStyle(root).getPropertyValue("--header-text");
         plusSVG.style.stroke = getComputedStyle(root).getPropertyValue("--header-text");
     });
