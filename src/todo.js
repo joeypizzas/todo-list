3fd7f32c7e63f90e9b1629762c2817bc464e7581
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-08-24 14:00:02 -0400

    Generate unique ID for each todo

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,14 @@
 // To-do class
 
 export class ToDo { // Instantiates new todos and has all methods to keep them up to date 
-    constructor(name, dueDate, description, isComplete, list) {
+    constructor(name, dueDate, description, isComplete, list, id) {
         this.name = name;
         this.dueDate = dueDate;
         this.description = description;
         this.isComplete = isComplete;
         this.list = list;
+        this.id = id || Date.now().toString(36) + Math.random().toString(36).substring(2, 8);
+
     }
 
     editName(updatedName) {
