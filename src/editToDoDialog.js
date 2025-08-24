a44fa4909ca7518704ac9c52003ca3789a228978
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-08-24 14:59:18 -0400

    Added logic to pre-fill selected todo info in modal

--- src/editToDoDialog.js
+++ src/editToDoDialog.js
@@ -1,17 +1,33 @@
 // JS for edit to-do modal
 
+import { lists } from "./list.js";
+
 const editToDoDialog = document.querySelector("#edit-todo-dialog");
 const lhn = document.querySelector("#lhn");
 const header = document.querySelector("#header");
 const main = document.querySelector("#main");
 const footer = document.querySelector("#footer");
+const toDoName = document.querySelector("#todo-name");
+const toDoDescription = document.querySelector("#todo-description");
+const toDoDue = document.querySelector("#todo-due");
+const toDoList = document.querySelector("#todo-list");
 
-export function showEditToDoDialog() {
+export function showEditToDoDialog(clickedToDoId) {
     editToDoDialog.show();
     lhn.classList.add("blur");
     header.classList.add("blur");
     main.classList.add("blur");
     footer.classList.add("blur");
+
+    for (const list of lists.getAllLists()) {
+        for (const todo of list.toDos) {
+            if (clickedToDoId === todo.id) {
+               toDoName.value = todo.name;
+               toDoDescription.value = todo.description;
+               toDoDue.value = todo.dueDate;
+            }
+        }
+    }
 }
 
 export function hideEditToDoDialog() {
