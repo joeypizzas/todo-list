e17d712b9e2464be06a0a81c1b44075d44297953
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-09-05 08:27:29 -0400

    Started adding logic to add new todo when saving

--- src/newDialog.js
+++ src/newDialog.js
@@ -1,5 +1,11 @@
 // JS for new dialog
 
+import { lists } from "./list.js";
+import { List } from "./list.js";
+import { addHomePage, removeHomePage } from "./homeView.js";
+import { saveData } from "./storage.js";
+import { ToDo } from "./todo.js";
+
 const root = document.documentElement;
 const newDialog = document.querySelector("#new-dialog");
 const lhn = document.querySelector("#lhn");
@@ -8,6 +14,12 @@ const main = document.querySelector("#main");
 const footer = document.querySelector("#footer");
 const closeNewModalButton = document.querySelector("#close-new-modal-button");
 const newSave = document.querySelector("#new-save");
+const radioButtons = document.querySelector(".radio-buttons");
+const checkedRadioButton = radioButtons.querySelector("input[name='todo-or-list']:checked");
+const newToDoName = document.querySelector("#new-todo-name");
+const newToDoDescription = document.querySelector("#new-todo-description");
+const newToDoDue = document.querySelector("#new-todo-due");
+const newToDoList = document.querySelector("#new-todo-list");
 
 export function showNewDialog() {
     newDialog.showModal();
@@ -15,6 +27,13 @@ export function showNewDialog() {
     header.classList.add("blur");
     main.classList.add("blur");
     footer.classList.add("blur");
+
+    for (const list of lists.getAllLists()) {
+        const toDoListOption = document.createElement("option");
+        toDoListOption.value = list.name;
+        toDoListOption.textContent = list.name;
+        newToDoList.appendChild(toDoListOption);
+    }
 }
 
 function hideNewDialog() {
@@ -36,6 +55,7 @@ closeNewModalButton.addEventListener("mousedown", () => {
 });
 closeNewModalButton.addEventListener("mouseup", () => {
     closeNewModalButton.style.color = getComputedStyle(root).getPropertyValue("--header-hover");
+    newToDoList.replaceChildren();
     hideNewDialog();
 });
 
@@ -51,5 +71,23 @@ newSave.addEventListener("mousedown", () => {
 newSave.addEventListener("mouseup", () => {
     newSave.style.backgroundColor = getComputedStyle(root).getPropertyValue("--header-hover");
 
-    // Take logic to save dialog from edit modal, make it into shared function and call here
+    if (checkedRadioButton.value === "To-Do") { // typeerror here
+        const newToDo = new ToDo(newToDoName.value, newToDoDue.value, newToDoDescription.value, "No", newToDoList.value);
+        for (const list of lists.getAllLists()) {
+            if (list.name === newToDoList.value) {
+                list.addToDo(newToDo);
+            }
+        }
+    }
+    
+    // Check whether todo or list
+    // if todo, get values of all fields and create new todo, then locate matching list and add it 
+    // if list, create new list 
+    // replace inputs, save data, remove home page, add home page, hide dialog
+
+    newToDoList.replaceChildren();
+    saveData();
+    removeHomePage();
+    addHomePage();
+    hideEditToDoDialog();
 });
